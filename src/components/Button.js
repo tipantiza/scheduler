112d1245d2480aacf4386f2d66af9d5ae9628350
Landon Germain Tipantiza <landontipantiza@landons-mbp.gateway.sasktel.net>
2020-07-06 20:52:06 -0600

    created day list and day list item in isolation then brought it into our app

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+const classNames = require('classnames');
 
 export default function Button(props) {
-   return <></>;
-}
+   const btnClass = classNames("button",{
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   })
+   return (
+      <button 
+         className={btnClass}
+         disabled={props.disabled} 
+         onClick={props.onClick} 
+      >
+         {props.children}
+      </button>
+   );
+ }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+
+const classNames = require("classnames")
+
+export default function DayListItem(props) {
+  const dayClass = classNames("day-list__item",{
+   "day-list__item--selected": props.selected,
+   "day-list__item--full": props.spots === 0
+
+  })
+
+  const formatSpots = (spot)=>{
+    switch (spot) {
+      case 0:
+        return "no spots remaining"
+      case 1:
+        return "1 spot remaining"
+      default:
+        return `${spot} spots remaining`
+    }
+  }
+  return (
+    <li className={dayClass} onClick={()=> props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
